3d790d6e9062deccbea450df5cfea28ef21b048b
TaeYeon_Kim <107397675+TAEYEN02@users.noreply.github.com>
2025-03-23 11:08:13 +0900

    note data-src 추가

--- my_page/js/home.js
+++ my_page/js/home.js
@@ -5,9 +5,14 @@ document.querySelector("#DATE").addEventListener("click", function () {
 
   document.querySelector("#HOME").addEventListener("click", function () {
     const url = this.getAttribute("data-src");
-    window.location.href = url; // 클릭하면 /date.html로 이동
+    window.location.href = url; // 클릭하면 /home.html로 이동
   });
   document.querySelector("#NOTE").addEventListener("click", function () {
     const url = this.getAttribute("data-src");
-    window.location.href = url; // 클릭하면 /date.html로 이동
+    window.location.href = url; // 클릭하면 /note.html로 이동
+  });
+
+  document.querySelector("#TODO").addEventListener("click", function () {
+    const url = this.getAttribute("data-src");
+    window.location.href = url; // 클릭하면 /todo.html로 이동
   });
